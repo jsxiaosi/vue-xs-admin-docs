a51671ee0fec54d681a642b118123478f36ccee4
小斯（xiaosi） <1531733886@qq.com>
2023-08-17 22:59:52 +0800

    feat: ✨ 添加脚本

---
+++ script/gitee.sh
@@ -0,0 +1,11 @@
+git checkout gitee-page
+
+git merge main --no-edit
+
+npm run docs:build
+
+git add .
+
+git commit -m 'feat: ✨ build'
+
+git push git@gitee.com:jsxiaosi/vue-xs-admin-docs.git gitee-page
